66c0f39ef05e9a2e6e1eb3f9555c9d4daccc8fd6
Tengill91 <robban_t@hotmail.com>
2022-11-29 16:18:51 +0100

    added ImageInput

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Switch, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
-import AppButton from "./app/components/AppButton";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingsEditScreen from "./app/screens/ListingsEditScreen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  const [category, setCategory] = useState();
+  const [imageUri, setImageUri] = useState();
 
-  return <ListingsEditScreen />;
+
+
+
+
+  return (
+    <Screen>
+      <ImageInput imageUri={imageUri} onChangeImage={(uri) => setImageUri(uri)}/>
+    </Screen>
+  );
 }
