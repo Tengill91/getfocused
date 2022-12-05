d3c7b65c2df8c8c9f3ee4b9e5dd604405064aaae
Tengill91 <robban_t@hotmail.com>
2022-12-05 14:03:05 +0100

    apisause

---
+++ app/api/client.js
@@ -0,0 +1,12 @@
+import { create } from "apisauce";
+
+const apiClient = create({
+  baseURL: "http://192.168.0.14.9000/api",
+});
+
+apiClient.get("/listings").then((response) => {
+  if (!response.ok) {
+    // standadiserade errors
+    response.problem;
+  }
+});
